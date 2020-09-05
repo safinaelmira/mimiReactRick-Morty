237e72ff19fb333a48f9a224109b091b8069ab9f
Elmira <corpse.17@mail.ru>
2020-09-06 00:43:46 +0500

    Updates

---
+++ precache-manifest.ddb37df74a44ed36c049cdb51f50a0bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e53e3c25e9178f9b3a81c4a3670844",
+    "url": "/mimiReactRick-Morty/index.html"
+  },
+  {
+    "revision": "008982caebd3bcd59509",
+    "url": "/mimiReactRick-Morty/static/css/main.11e60269.chunk.css"
+  },
+  {
+    "revision": "9569ce97444fdb530d38",
+    "url": "/mimiReactRick-Morty/static/js/2.256082df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mimiReactRick-Morty/static/js/2.256082df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "008982caebd3bcd59509",
+    "url": "/mimiReactRick-Morty/static/js/main.8b23231c.chunk.js"
+  },
+  {
+    "revision": "8af097207478975a12c2",
+    "url": "/mimiReactRick-Morty/static/js/runtime-main.88a12548.js"
+  },
+  {
+    "revision": "5c1a1d7996d5085a5dddc758c66b60a6",
+    "url": "/mimiReactRick-Morty/static/media/bg.5c1a1d79.jpg"
+  }
+]);
